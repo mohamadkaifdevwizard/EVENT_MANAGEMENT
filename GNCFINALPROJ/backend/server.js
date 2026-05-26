7f7d3c90a72c6b40c48c7d3897c7dff752eb4db3
Mohamad kaif <wesafe8@gmail.com>
2026-05-27 01:53:48 +0530

    Backend setup completed

--- GNCFINALPROJ/backend/server.js
+++ GNCFINALPROJ/backend/server.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 dotenv.config();
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Test Route
 app.get("/", (req, res) => {
   res.send("Event Management Backend Running");
 });
