c427dadeae76f313c9e1bb43581403c2da5fa846
Mohamad kaif <wesafe8@gmail.com>
2026-06-04 20:08:32 +0530

    Completed backend API testing

--- GNCFINALPROJ/backend/server.js
+++ GNCFINALPROJ/backend/server.js
@@ -2,16 +2,24 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const dns = require("dns");
+
+// Force Google DNS
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const authRoutes = require("./routes/authRoutes");
-
 const eventRoutes = require("./routes/eventRoutes");
 
 dotenv.config();
+
 console.log(process.env.MONGO_URI);
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log(err));
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    family: 4,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 const app = express();
 
@@ -20,7 +28,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-
 app.use("/api/events", eventRoutes);
 
 // Test Route
